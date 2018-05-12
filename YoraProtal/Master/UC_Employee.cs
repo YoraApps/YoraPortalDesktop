b273e9c18ff0823b9830e71881f947ca44eb3f64
akash <aka01sh@gmail.com>
2018-05-12 16:36:54 +0530

    emp design change

--- YoraProtal/Master/UC_Employee.cs
+++ YoraProtal/Master/UC_Employee.cs
@@ -16,5 +16,35 @@ namespace YoraProtal.Master
         {
             InitializeComponent();
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
